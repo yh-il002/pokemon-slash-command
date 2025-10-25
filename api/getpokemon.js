d6bcff550bfe7fa16d6eeaa0c1eb0f661c5a00b9
平松泰明 <yhiramatsu@ntv-wands.co.jp>
2025-10-25 11:34:39 +0900

    ts→jsに変更

--- api/getpokemon.js
+++ api/getpokemon.js
@@ -1,22 +1,19 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  // SlackのSlash Commandは基本POSTで飛んでくる
+export default function handler(req, res) {
+  // SlackのSlash Commandは基本POST
   if (req.method !== "POST") {
     res.status(405).send("Method Not Allowed");
     return;
   }
 
-  // 1〜1025でランダムな整数
+  // 1〜1025のランダム整数
   const min = 1;
   const max = 1025;
   const id = Math.floor(Math.random() * (max - min + 1)) + min;
 
-  // 画像URL
+  // スプライト画像URL
   const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
 
-  // Slackに返すJSON
-  // blocksを使って画像をどーんと出す（in_channelでチャンネル全員に見せる）
+  // Slackに返すメッセージ（Block Kit）
   const body = {
     response_type: "in_channel",
     blocks: [
